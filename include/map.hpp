6d47780c34cf9687c72fb5f0dd796e96867c5981
Laura CAVALLUCCI <lcavallu@e1r3p16.clusters.42paris.fr>
2022-08-19 15:14:09 +0200

    cc on commence map

---
+++ include/map.hpp
@@ -0,0 +1,35 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+#include <iostream>
+#include "vector.hpp"
+#include "map_iterator.hpp"
+
+namespace ft
+{
+	template < class Key
+     , class T
+     , class Compare = std::less<Key>
+     , class Allocator = std::allocator<std::pair<const Key, T>>>
+    class map
+	{
+        public :
+
+        typedef Key                                 key_type;
+        typedef T                                   mapped_type;
+        typedef pair<const key_type,mapped_type>    value_type;
+        typedef Compare                             key_compare;
+                                                    value_compare;
+        typedef Allo                                allocator_type;
+        typedef allocator_type::reference           reference;
+        typedef allocator_type::const_reference     const_reference;
+        typedef allocator_type::pointer             pointer;
+        typedef allocator_type::const_pointer       const_pointer;
+        typedef allocator_type::difference_type     difference_type;
+        typedef allocator_type::size_type           size_type;
+                                                    iterator;
+                                                    const_iterator;
+                                                    reverse_iterator;
+                                                    const_reverse_iterator;
+    }
+}
---
+++ include/map_iterator.hpp
@@ -0,0 +1,9 @@
+ #ifndef MAP_ITERATOR_HPP
+ #define MAP_ITERATOR_HPP
+
+ #include "map.hpp"
+
+ namespace ft
+ {
+    
+ }
